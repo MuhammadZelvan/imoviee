701fc44c5dbcff81e506afa1230fdd3331457aa6
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2024-11-15 07:20:36 +0700

    Added Home, Schedule, History, Watchlist, and Profile page. Added navbar

---
+++ app/src/main/java/com/zelvan/imoviee/LayoutApp.kt
@@ -0,0 +1,59 @@
+package com.zelvan.imoviee
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class LayoutApp : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_layout_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        loadFragment(Home())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(Home())
+                    true
+                }
+                R.id.schedule -> {
+                    loadFragment(Schedule())
+                    true
+                }
+                R.id.history -> {
+                    loadFragment(History())
+                    true
+                }
+                R.id.watchlist -> {
+                    loadFragment(Watchlist())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(Profile())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+}
